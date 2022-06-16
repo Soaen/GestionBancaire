4f82fbdcbfaa980fede65cd95827f37c53df9fae
Soaen <sylvain.vansteenkiste59@gmail.com>
2022-06-16 11:55:52 +0200

    [Sylvain] Ajout Agence

---
+++ src/fr/example/beans/Agency.java
@@ -0,0 +1,19 @@
+package fr.example.beans;
+
+public class Agency {
+    int codeAgency;
+    String nameAgency;
+    String addressAgency;
+
+    public Agency(int codeAgency, String nameAgency, String addressAgency) {
+        this.codeAgency = codeAgency;
+        this.nameAgency = nameAgency;
+        this.addressAgency = addressAgency;
+    }
+
+    public int getCodeAgency() {
+        return codeAgency;
+    }
+
+    public Agency(){}
+}
--- src/fr/example/services/AgencyManagment.java
+++ src/fr/example/services/AgencyManagment.java
@@ -1,7 +1,27 @@
 package fr.example.services;
 
-public class AgencyManagment {
-    public void creerAgence(){
+import fr.example.beans.Agency;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AgencyManagment {
+    ArrayList<Agency> agencyList = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+    Agency ag = new Agency();
+    public void creerAgence(){
+        System.out.println("Entrez un code Agence disponible");
+        int agencyCode = sc.nextInt();
+        while(ag.getCodeAgency() == agencyCode){
+            System.out.println("Code agence déjà utilisé ! choisissez en un autre !");
+            System.out.println("Entrez un code Agence disponible");
+            agencyCode = sc.nextInt();
+        }
+        sc.nextLine();
+        System.out.println("Quel est le nom de l'agence ?");
+        String nameAgency = sc.nextLine();
+        System.out.println("Quel est l'adresse de l'agence ?");
+        String addressAgency = sc.nextLine();
+        agencyList.add(new Agency(agencyCode, nameAgency, addressAgency));
     }
 }
