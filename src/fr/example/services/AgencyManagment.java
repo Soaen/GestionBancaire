29c6d7a3525d0b2dec024113cecc99e4f97f0427
Soaen <sylvain.vansteenkiste59@gmail.com>
2022-06-16 11:30:06 +0200

    [Sylvain] Managment

---
+++ src/fr/example/services/AgencyManagment.java
@@ -0,0 +1,7 @@
+package fr.example.services;
+
+public class AgencyManagment {
+    public void creerAgence(){
+
+    }
+}
---
+++ src/fr/example/services/ServiceManagment.java
@@ -0,0 +1,5 @@
+package fr.example.services;
+
+public class ServiceManagment {
+
+}
